cc920f34676684260c77188987208b9ecc49efca
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2023-05-15 14:08:53 +0200

    update

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 include 'header.php';
 include 'nav.php';
 ?>
-
-<h1>Nous allons retrouver les exercices en cours de back-end</h1>
-
+<main class="index">
+    <h1>Bienvenue sur la page des exercices de back-end</h1>
+    <img src="https://cdn.pixabay.com/photo/2014/12/30/10/39/code-583795__340.jpg" alt="">
+</main>
 <?php
 include 'footer.php';
 ?>
--- nav.php
+++ nav.php
@@ -1,47 +1,46 @@
 <nav>
     <div class="container">
-        <div class="navbrand">Forma DWWM - PHP</div>
-        <ul>
-            <a href="Exo0.php">
-                <li>Exo 0</li>
-            </a>
-            <a href="Exo1.php">
-                <li>Exo 1</li>
-            </a>
-            <a href="Exo2.php">
-                <li>Exo 2</li>
-            </a>
-            <a href="Exo3.php">
-                <li>Exo 3</li>
-            </a>
-            <a href="Exo4.php">
-                <li>Exo 4</li>
-            </a>
-            <a href="Exo5.php">
-                <li>Exo 5</li>
-            </a>
-            <a href="Exo6.php">
-                <li>Exo 6</li>
-            </a>
-            <a href="Exo7.php">
-                <li>Exo 7</li>
-            </a>
-            <a href="Exo8.php">
-                <li>Exo 8</li>
-            </a>
-            <a href="Exo9.php">
-                <li>Exo 9</li>
-            </a>
-            <a href="POO1.php">
-                <li>POO1</li>
-            </a>
-            <a href="login.php">
-                <li>LOGIN</li>
-            </a>
-            <div class="separation"></div>
-            <a href="logout.php">
-                <li>Deconnexion</li>
-            </a>
+        <div class="navbrand"><a href="index.php">Forma DWWM - PHP</a></div>
+        <ul class="menu">
+            <li class="dropdown">EXERCICES
+                <ul class="dropdown-content">
+                    <a href=" Exo0.php">
+                        <li>Exo 0</li>
+                    </a>
+                    <a href="Exo1.php">
+                        <li>Exo 1</li>
+                    </a>
+                    <a href="Exo2.php">
+                        <li>Exo 2</li>
+                    </a>
+                    <a href="Exo3.php">
+                        <li>Exo 3</li>
+                    </a>
+                    <a href="Exo4.php">
+                        <li>Exo 4</li>
+                    </a>
+                    <a href="Exo5.php">
+                        <li>Exo 5</li>
+                    </a>
+                    <a href="Exo6.php">
+                        <li>Exo 6</li>
+                    </a>
+                    <a href="Exo7.php">
+                        <li>Exo 7</li>
+                    </a>
+                    <a href="Exo8.php">
+                        <li>Exo 8</li>
+                    </a>
+                    <a href="Exo9.php">
+                        <li>Exo 9</li>
+                    </a>
+
+                </ul>
+            </li>
+            <li><a href="POO1.php">POO</a></li>
+            <li><a href="login.php"><i class="fa-solid fa-user" style="color: #ffffff;"></i></a></li>
+            <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i></a></li>
+
         </ul>
     </div>
 </nav>
