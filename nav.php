21efb48584b53f5080c9222866ea8cad3d94d680
Ste[PH]ane <steph.guery@gmail.com>
2023-05-15 08:25:01 +0200

    Changement de style

--- nav.php
+++ nav.php
@@ -3,41 +3,42 @@
         <div class="navbrand">Forma DWWM - PHP</div>
         <ul>
             <a href="Exo0.php">
-                <li>Exo 0 |</li>
+                <li>Exo 0</li>
             </a>
             <a href="Exo1.php">
-                <li>Exo 1 |</li>
+                <li>Exo 1</li>
             </a>
             <a href="Exo2.php">
-                <li>Exo 2 |</li>
+                <li>Exo 2</li>
             </a>
             <a href="Exo3.php">
-                <li>Exo 3 |</li>
+                <li>Exo 3</li>
             </a>
             <a href="Exo4.php">
-                <li>Exo 4 |</li>
+                <li>Exo 4</li>
             </a>
             <a href="Exo5.php">
-                <li>Exo 5 |</li>
+                <li>Exo 5</li>
             </a>
             <a href="Exo6.php">
-                <li>Exo 6 |</li>
+                <li>Exo 6</li>
             </a>
             <a href="Exo7.php">
-                <li>Exo 7 |</li>
+                <li>Exo 7</li>
             </a>
             <a href="Exo8.php">
-                <li>Exo 8 |</li>
+                <li>Exo 8</li>
             </a>
             <a href="Exo9.php">
-                <li>Exo 9 |</li>
+                <li>Exo 9</li>
             </a>
             <a href="POO1.php">
-                <li>POO1 |</li>
+                <li>POO1</li>
             </a>
             <a href="login.php">
-                <li>LOGIN |</li>
+                <li>LOGIN</li>
             </a>
+            <div class="separation"></div>
             <a href="logout.php">
                 <li>Deconnexion</li>
             </a>
